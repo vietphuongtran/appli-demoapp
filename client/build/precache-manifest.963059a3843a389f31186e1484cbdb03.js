a57dbcc712ea41afcee32e5a74ae62b8f0b31326
Paul Tran <paultran0307@gmail.com>
2020-06-30 08:05:07 -0400

    Add files via upload

---
+++ client/build/precache-manifest.963059a3843a389f31186e1484cbdb03.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "619e0274d31e2b049a0ead5dca9fa66c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef8822fdbc31da17c27a",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "df0c0607b64448c9dd70",
+    "url": "/static/css/main.9af0d860.chunk.css"
+  },
+  {
+    "revision": "ef8822fdbc31da17c27a",
+    "url": "/static/js/2.eee3e4ca.chunk.js"
+  },
+  {
+    "revision": "4386012a6d334684338618496b7aac20",
+    "url": "/static/js/2.eee3e4ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df0c0607b64448c9dd70",
+    "url": "/static/js/main.65731d61.chunk.js"
+  },
+  {
+    "revision": "9d9fd87fcbc52b6f8b25",
+    "url": "/static/js/runtime-main.a4a02d76.js"
+  }
+]);
